58612cff97567b64a9408ed725d0ae5c2fd05b6d
Walter Hulsebos <walterhulsebos@outlook.com>
2021-03-10 13:27:57 +0100

    Basic World methods

--- ECStasy/World.h
+++ ECStasy/World.h
@@ -1,11 +1,45 @@
-#ifndef ECSTASY_WORLD_H
-#define ECSTASY_WORLD_H
+#pragma once
 
+#include <Ecstasy/Object.h>
 
-class World
+namespace Ecstasy
 {
-
-};
-
-
-#endif //ECSTASY_WORLD_H
+    class World
+    {
+    public:
+        World();
+        
+        ~World();
+        
+        #pragma region Objects
+        
+        Object CreateObject() noexcept;
+        
+        template<typename T, typename ... Args>
+        Object CreateObject() noexcept
+        {
+            //TODO: Implement.
+        }
+        
+        bool DestroyObject(const Object& object);
+        
+        #pragma endregion
+        
+        #pragma region Datas
+        
+        //Only works if T_Data has an empty constructor variant.
+        template<typename T_Data, typename std::enable_if<std::is_default_constructible<T>::value, int>::type = 0>
+        void AddData(Object& object)
+        {
+        
+        }
+        
+        template<typename T_Data>
+        void AddData(Object& object, T_Data data)
+        {
+        
+        }
+        
+        #pragma endregion
+    };
+}
