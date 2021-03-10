992a17a37b4637faceae9bccc4ba6b66cfb4aeee
Walter Hulsebos <walterhulsebos@outlook.com>
2021-03-10 12:26:05 +0100

    Finished Object

--- ECStasy/Object.h
+++ ECStasy/Object.h
@@ -1,5 +1,4 @@
-#ifndef ECSTASY_OBJECT_H
-#define ECSTASY_OBJECT_H
+#pragma once
 
 namespace Ecstasy
 {
@@ -7,11 +6,25 @@ namespace Ecstasy
     ///Calling the Entities Objects because as far as I'm planning right now it won't have the traditional ECS features like parallelism.
     ///Because of that I won't be calling it an Entity Component System, there for the Entities Components and Systems will have different names to avoid confusion.
     
-    class Object
+    class Object final
     {
+    public:
+        uint32_t id;
+        
+        /**
+         * Needed for pooling, when you "destroy" an Object it'll be recycled.
+         * The ID won't be reset, only the version will be updated.
+         */
+        uint32_t version;
     
+    public:
+        Object();
+        Object(const uint32_t id, const uint32_t version);
+        Object(const Object& other);
+    
+    public:
+        bool operator==(const Object& other) const;
+        bool operator!=(const Object& other) const;
     };
     
 }
-
-#endif //ECSTASY_OBJECT_H
