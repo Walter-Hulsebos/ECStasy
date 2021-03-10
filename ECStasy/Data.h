463fcf3504e62467c4d6bc46ee378a04ffe08a61
Walter Hulsebos <walterhulsebos@outlook.com>
2021-03-10 12:33:26 +0100

    Finished Data (so far)

--- ECStasy/Data.h
+++ ECStasy/Data.h
@@ -1,11 +1,8 @@
-#ifndef ECSTASY_DATA_H
-#define ECSTASY_DATA_H
+#pragma once
 
-
+/// Represents any sort of Component available to an entity.
 class Data
 {
-
+public:
+    virtual ~Component() = default;
 };
-
-
-#endif //ECSTASY_DATA_H
