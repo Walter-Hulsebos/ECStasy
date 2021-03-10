4d93047d78dc7439ce77ffb529ed370539ae438d
Walter Hulsebos <walterhulsebos@outlook.com>
2021-03-10 11:49:54 +0100

    Start of Objects

---
+++ ECStasy/Object.h
@@ -0,0 +1,17 @@
+#ifndef ECSTASY_OBJECT_H
+#define ECSTASY_OBJECT_H
+
+namespace Ecstasy
+{
+    
+    ///Calling the Entities Objects because as far as I'm planning right now it won't have the traditional ECS features like parallelism.
+    ///Because of that I won't be calling it an Entity Component System, there for the Entities Components and Systems will have different names to avoid confusion.
+    
+    class Object
+    {
+    
+    };
+    
+}
+
+#endif //ECSTASY_OBJECT_H
